14507133f25ac7eb34d7ea2fb49cddddf6939c0a
Bruno Rafael Sant' Ana <bruno.fullstackdeveloper@gmail.com>
2019-11-15 20:37:32 -0300

    First commit with empty project.

---
+++ src/main/java/me/brunosantana/headfirst/design_patterns/App.java
@@ -0,0 +1,13 @@
+package me.brunosantana.headfirst.design_patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
