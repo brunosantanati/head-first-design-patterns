eb11d93bcdc301c6955ae778cee7cdfc577177b6
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2019-12-10 23:05:25 -0300

    Example of Simple Factory(It is NOT a design pattern)

---
+++ src/main/java/me/brunosantana/headfirst/design_patterns/simple_factory/ClamPizza.java
@@ -0,0 +1,11 @@
+package me.brunosantana.headfirst.design_patterns.simple_factory;
+
+public class ClamPizza extends Pizza {
+	public ClamPizza() {
+		name = "Clam Pizza";
+		dough = "Thin crust";
+		sauce = "White garlic sauce";
+		toppings.add("Clams");
+		toppings.add("Grated parmesan cheese");
+	}
+}
---
+++ src/main/java/me/brunosantana/headfirst/design_patterns/simple_factory/PepperoniPizza.java
@@ -0,0 +1,12 @@
+package me.brunosantana.headfirst.design_patterns.simple_factory;
+
+public class PepperoniPizza extends Pizza {
+	public PepperoniPizza() {
+		name = "Pepperoni Pizza";
+		dough = "Crust";
+		sauce = "Marinara sauce";
+		toppings.add("Sliced Pepperoni");
+		toppings.add("Sliced Onion");
+		toppings.add("Grated parmesan cheese");
+	}
+}
