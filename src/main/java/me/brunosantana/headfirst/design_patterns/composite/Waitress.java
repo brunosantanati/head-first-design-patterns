a5db4b6f93e4786790053fab016f638a1feb8c91
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-02-09 00:07:32 -0300

    Example of Composite pattern.

---
+++ src/main/java/me/brunosantana/headfirst/design_patterns/composite/Waitress.java
@@ -0,0 +1,13 @@
+package me.brunosantana.headfirst.design_patterns.composite;
+  
+public class Waitress {
+	MenuComponent allMenus;
+ 
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	}
+ 
+	public void printMenu() {
+		allMenus.print();
+	}
+}
